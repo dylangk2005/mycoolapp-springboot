28a5fc90f16741b251e4363dc8b0691617668802
dylangk2005 <phamdinhhai.dev@gmail.com>
2026-02-17 19:22:50 +0700

    feat : add some information in application.properties chore: remove actuator and security dependencies for learning purpose

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,22 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    // inject properties for my properties
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "team info"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "The team is called: " + teamName + " and the coach name: " + coachName;
+    }
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello() {
