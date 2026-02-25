8ae2bb4eae36ba572f73c046831884b6c50309e9
dylangk2005 <phamdinhhai.dev@gmail.com>
2026-02-25 21:13:50 +0700

    feat: (03) saving a java object with JPA

--- 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/dylangk/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/dylangk/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.dylangk.cruddemo;
 
+import com.dylangk.cruddemo.dao.StudentDAO;
+import com.dylangk.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,34 @@ public class CruddemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(){
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO
+    ){
         return runner ->{
-          System.out.println("Hello World");
+           createMultipleStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        // create the student object
+        System.out.println("Creating new student object....");
+        Student tempStudent = new Student("Dylann", "Kim", "dylan@gmail.com");
+        // save the student object
+        studentDAO.save(tempStudent);
+
+    }
+
+    private void createMultipleStudent(StudentDAO studentDAO) {
+        // create multiple students
+        System.out.println("Creating 3 students....");
+        Student tempStudent1 = new Student("John", "Cena", "john@gmail.com");
+        Student tempStudent2 = new Student("Dean", "Ambrose", "dean@gmail.com");
+        Student tempStudent3 = new Student("Roman", "Reigns", "roman@gmail.com");
+
+        // save the student object
+        System.out.println("Saving students....");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
+
     }
 }
---
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/dylangk/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.dylangk.cruddemo.dao;
+
+import com.dylangk.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student student);
+
+}
