a55df2fb2303facd276fef487069f78c028b5fdd
dylangk2005 <phamdinhhai.dev@gmail.com>
2026-02-27 16:57:38 +0700

    chore: (04) setting up project

---
+++ 04-spring-boot-rest-crud/restdemo/src/main/java/com/dylangk/restdemo/rest/DemoRestController.java
@@ -0,0 +1,15 @@
+package com.dylangk.restdemo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+    // add code for ther "/hello endpoint"
+    @GetMapping("/hello")
+    public String sayhello() {
+        return "Hello World!";
+    }
+}
