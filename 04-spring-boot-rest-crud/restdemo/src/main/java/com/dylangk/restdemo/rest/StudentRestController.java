c3df142748b821a002e88df8235eb699c8fe65bf
dylangk2005 <phamdinhhai.dev@gmail.com>
2026-02-28 20:58:12 +0700

    chore: (04) Create student list during web initialization instead of recreating it on each endpoint call

--- 04-spring-boot-rest-crud/restdemo/src/main/java/com/dylangk/restdemo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/restdemo/src/main/java/com/dylangk/restdemo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.dylangk.restdemo.rest;
 
 import com.dylangk.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,19 +13,22 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    //define endpoint for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents(){
+    private List<Student> students;
 
-        List<Student> students = new ArrayList<>();
-
+    // define @PostConstruc to load the student data .. only once!
+    @PostConstruct
+    public void init(){
+        students = new ArrayList<>();
         students.add(new Student("John", "Doe"));
         students.add(new Student("Jane", "Doe"));
         students.add(new Student("Bob", "Smith"));
         students.add(new Student("Alice", "Johnson"));
         students.add(new Student("Charlie", "Williams"));
+    }
 
-
+    //define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return students;
     }
 }
