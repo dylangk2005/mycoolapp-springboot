31e79925a09c21ee0b6b89464c87f171b5b3d380
dylangk2005 <phamdinhhai.dev@gmail.com>
2026-02-28 21:07:02 +0700

    feat: (04) Spring Boot Path Variables

--- 04-spring-boot-rest-crud/restdemo/src/main/java/com/dylangk/restdemo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/restdemo/src/main/java/com/dylangk/restdemo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.dylangk.restdemo.rest;
 import com.dylangk.restdemo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class StudentRestController {
 
     private List<Student> students;
 
-    // define @PostConstruc to load the student data .. only once!
+    // define @PostConstruct to load the student data .. only once!
     @PostConstruct
     public void init(){
         students = new ArrayList<>();
@@ -24,6 +25,12 @@ public class StudentRestController {
         students.add(new Student("Bob", "Smith"));
         students.add(new Student("Alice", "Johnson"));
         students.add(new Student("Charlie", "Williams"));
+    }
+
+    // define endpoint or "student/{studentid}" - return student at index
+    @GetMapping("/students/{studentid}")
+    public Student getStudent(@PathVariable int studentid){
+        return students.get(studentid);
     }
 
     //define endpoint for "/students" - return a list of students
