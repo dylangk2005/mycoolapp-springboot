987d483e54595fa6bf8f9201173c09d5b95efa9d
dylangk2005 <phamdinhhai.dev@gmail.com>
2026-03-01 17:56:11 +0700

    feat: (04) Spring Boot REST Exception Handling

--- 04-spring-boot-rest-crud/restdemo/src/main/java/com/dylangk/restdemo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/restdemo/src/main/java/com/dylangk/restdemo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.dylangk.restdemo.rest;
 
 import com.dylangk.restdemo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,9 +29,39 @@ public class StudentRestController {
     // define endpoint or "student/{studentid}" - return student at index
     @GetMapping("/students/{studentid}")
     public Student getStudent(@PathVariable int studentid){
+        // check the studentId again list size
+        if(studentid >= students.size() || studentid < 0){
+            throw new StudentNotFoundException("Student not found");
+        }
         return students.get(studentid);
     }
 
+    // Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        // response ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        // response ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
     //define endpoint for "/students" - return a list of students
     @GetMapping("/students")
     public List<Student> getStudents(){
